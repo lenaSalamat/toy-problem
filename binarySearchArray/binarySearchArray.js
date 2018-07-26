df33adbf8c22b50b77661f9283f1b7580bd951a6
lenaSalamat <lenasalamat96@gmail.com>
2018-07-26 19:42:26 +0300

    toy problem

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,6 +11,23 @@
  */
 
 var binarySearch = function (array, target) {
-  
+  var half_length = Math.ceil(array.length / 2);
+  var leftSide = array.splice(0,half_length);
+  var rightSide = leftSide.concat(array);
+    if(target <= half_length ){
+      for(var j = 0 ; j< leftSide.length ; j++){
+      	if(target === leftSide[j]){
+      		return j;
+        }
+      }
+    }else{
+   if(target > half_length){
+     for(var i = 0 ; i < rightSide.length ; i++){
+        if(target === rightSide[i]){
+           return i;
+      }
+    }
+  }
+}
+  return -1;
 };
-
