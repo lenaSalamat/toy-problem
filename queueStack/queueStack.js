3e839d197cbd3b52a3e298b301868ef163c5ef9d
lenaSalamat <lenasalamat96@gmail.com>
2018-07-30 19:08:03 +0300

    6toy problem

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -4,35 +4,46 @@
 
 
 var Stack = function() {
-	var storage = [];
-
-	this.push = function(){
-
+  var storage = [];
+  var counter = 0;
+	this.push = function(value){
+      storage[counter++] = value ;
 	};
 
 	this.pop = function(){
-
+      var newval = storage[counter - 1];
+      delete storage[counter - 1];
+      counter--;
+      return newval;
 	};
 
 	this.size = function(){
-
+      return storage.length;
 	};
 };
 
 var Queue = function() {
 
-	var inbox = new Stack();
-	var outbox = new Stack();
+  var inbox = new Stack();
+  var outbox = new Stack();
 
-	this.enqueue = function(){
-
+	this.enqueue = function(value){
+        inbox.push(value);
 	};
 
 	this.dequeue = function(){
-		
+	  while ( inbox.size() ) {
+        var value = inbox.pop()
+          outbox.push(value)
+    }
+      while( outbox.size() ) {
+        var val = outbox.pop()
+          inbox.push(val);
+    }
+      return val;
 	};
 
 	this.size = function(){
-
+      return inbox.size();
 	};
 };
