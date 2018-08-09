48489ed22b1263b782aa547691e2e2913794410a
lenaSalamat <lenasalamat96@gmail.com>
2018-08-09 19:49:51 +0300

    wordsWthinWords

--- wordsWthinWords/wordsWthinWords.js
+++ wordsWthinWords/wordsWthinWords.js
@@ -1,8 +1,24 @@
 //Words within Words
-//Given an array of unique words, find the word that contains the greatest number of other words. 
+//Given an array of unique words, find the word that 
+//contains the greatest number of other words. 
 //A word must be at least two letters long.
 //input:
 //wordList:
 //[ "gray", "grays", "ray", "rays", "strays" ]	
 //output:"grays"
 
+
+function wordsWthinWords (array){
+  var count = 0;
+//var x=array.split("");
+  var longest=array;
+for(var i=0; i < array.length; i++){
+for(var j=0;j<array.length;j++){
+    if(array[i].length > count && array[i].match(array[j]) && array[i].length>array[j].length){
+      var count = array[i].length;
+        longest = array[i];
+      }      
+   }
+       }
+ return longest;
+} 
